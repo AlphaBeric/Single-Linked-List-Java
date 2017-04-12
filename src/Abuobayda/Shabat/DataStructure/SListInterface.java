20831d43b742786dd3af324229f1f5353ebc9ed1
absha <absha@DESKTOP-0KL3UBP>
2017-04-12 16:52:30 +0200

    Build SlistInterface

---
+++ src/Abuobayda/Shabat/DataStructure/SListInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abuobayda.Shabat.DataStructure;
+
+/**
+ *
+ * @author Abuobayda Shabat
+ */
+public interface SListInterface<T> {
+    public void addToFront(T item);
+    public T removeFront();
+    public void addToBack(T item);
+    public T removeBack();
+    public ListNode contain(int position);
+    public boolean isEmpty();
+    public void clear();
+    
+}
