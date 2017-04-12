353bc2bb8dd2fd958b0cc540f750ffe4058783ab
absha <absha@DESKTOP-0KL3UBP>
2017-04-12 02:46:32 +0200

    Implementing the ListNode class and the NodeInterface methods

--- src/Abuobayda/Shabat/DataStructure/ListNode.java
+++ src/Abuobayda/Shabat/DataStructure/ListNode.java
@@ -8,6 +8,44 @@ package Abuobayda.Shabat.DataStructure;
  *
  * @author absha
  */
-public class ListNode <T>{
+public class ListNode <T> implements NodeInterface{
+    
+    //Create two fields item and next
+    private T item;
+    private ListNode next;
+    
+    //Constructor for the first and the last node
+    //where the next will be null
+    ListNode(T value){
+        this(value, null);
+    }
+    
+    //Constructor for any node that have 
+    //item and a reference to the next node
+    ListNode(T value, ListNode n){
+        item = value;
+        next = n;
+    }
+    
+    @Override
+    public void setNext(ListNode n) {
+        next = n;
+    }
+
+    @Override
+    public ListNode getNext() {
+        return next;
+    }
+
+    @Override
+    public void setItem(Object item) {
+        this.item = (T) item;
+    }
+
+    @Override
+    public Object getItem() {
+        return (T) item;
+    }
+    
     
 }
--- src/Abuobayda/Shabat/DataStructure/NodeInterface.java
+++ src/Abuobayda/Shabat/DataStructure/NodeInterface.java
@@ -9,9 +9,13 @@ package Abuobayda.Shabat.DataStructure;
  * @author absha
  */
 public interface NodeInterface<T> {
+    //Change the next node reference
     public void setNext(ListNode n);
+    //return a reference of the next node
     public ListNode getNext();
+    //change the value on anode
     public void setItem(T item);
+    //return the item from a particular node
     public T getItem();
     
     
